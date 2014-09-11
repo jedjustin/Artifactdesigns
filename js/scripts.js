f1a484ec258406a3a049f291f1d5d1cfec2feadf
artifact <jedjustingumiran@gmail.com>
2014-09-10 17:33:09 -0700

    added preloader

--- js/scripts.js
+++ js/scripts.js
@@ -323,3 +323,12 @@ function parallaxed(obj) {
 
 }
 
+jQuery(document).ready(function($) {  
+
+// site preloader -- also uncomment the div in the header and the css style for #preloader
+$(window).load(function(){
+	$('#preloader').fadeOut('slow',function(){$(this).remove();});
+});
+
+});
+
